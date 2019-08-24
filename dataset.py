0d54e3489bb4e10246edc5b40124429ab4a1b402
Rajas Kakodkar <rajaskakodkar16@gmail.com>
2019-08-24 18:09:17 +0530

    Update dataset.py

--- dataset.py
+++ dataset.py
@@ -57,7 +57,7 @@ class CustomDataset(Dataset):
         if self.phase != 'test':
             # filename of image should have 'id_label.jpg/png' form
             #label = int((self.data_list[item].split('.')[0]).split('_')[-1])  # label
-            label = int(re.findall(r'([\w]+)_[\d]+_[\d]+.jpg$',self.data_list[item])[0])
+            label = int(re.findall(r'[\w]+_([\d]+)_[\d]+.jpg$',self.data_list[item])[0])
             return image, label
         else:
             # filename of image should have 'id.jpg/png' form, and simply return filename in case of 'test'
